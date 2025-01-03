495de2aad41d36e605a470f15de72ffe2cb59432
Udara Neminda Chandrawansa <udaraneminda@gmail.com>
2025-01-03 21:23:09 +0530

    footer updated: missing components added from og

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -46,7 +46,7 @@ function Footer() {
   };
 
   return (
-    <div className="flex flex-col px-4 pt-12 mt-6 h-fit bg-gray-50 gap-14 max-md:gap-6 max-sm:gap-3">
+    <div className="flex flex-col gap-10 px-4 pt-12 mt-6 h-fit bg-gray-50 max-md:gap-6 max-sm:gap-3">
       {/*logo & shit*/}
       <div className="flex max-[992px]:flex-col">
         {/*logo + social*/}
@@ -60,8 +60,12 @@ function Footer() {
               <span className="border-l-gray-400 border-l ml-11 h-[40px]"></span>
             </div>
             {/*social media icons*/}
-            <div className="w-1/2 flex gap-3 items-center max-[992px]:pl-4">
-              <Link href="https://www.facebook.com/ncihomespvtltd/" className="h-fit">
+            <div className="w-1/2 flex gap-3 items-center max-[992px]:pl-4 max-md:w-fit">
+              Follow Us On
+              <Link
+                href="https://www.facebook.com/ncihomespvtltd/"
+                className="h-fit"
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
@@ -97,7 +101,10 @@ function Footer() {
                   <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.01 2.01 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.01 2.01 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31 31 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.01 2.01 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A100 100 0 0 1 7.858 2zM6.4 5.209v4.818l4.157-2.408z" />
                 </svg>
               </Link>
-              <Link href="https://www.instagram.com/explore/tags/ncihomes/" className="h-fit">
+              <Link
+                href="https://www.instagram.com/explore/tags/ncihomes/"
+                className="h-fit"
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
@@ -109,7 +116,10 @@ function Footer() {
                   <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.9 3.9 0 0 0-1.417.923A3.9 3.9 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.9 3.9 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.9 3.9 0 0 0-.923-1.417A3.9 3.9 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599s.453.546.598.92c.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.5 2.5 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.5 2.5 0 0 1-.92-.598 2.5 2.5 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233s.008-2.388.046-3.231c.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92s.546-.453.92-.598c.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92m-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217m0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334" />
                 </svg>
               </Link>
-              <Link href="https://www.linkedin.com/in/ncihomes/?originalSubdomain=lk" className="h-fit">
+              <Link
+                href="https://www.linkedin.com/in/ncihomes/?originalSubdomain=lk"
+                className="h-fit"
+              >
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   width="16"
@@ -142,12 +152,51 @@ function Footer() {
           ></Button>
         </div>
       </div>
-      {/*row 3*/}
-      <div className="flex h-fit">
-        <div className="h-full w-full pr-4 max-[992px]:px-4 pt-4 max-[992px]:w-full max-[992px]:pb-4 max-[992px]:border-b-[1px]">
+      {/*row 2*/}
+      <div className="flex h-fit max-[992px]:flex-col gap-4">
+        <div className="h-full w-1/2  max-[992px]:px-4 pt-4 max-[992px]:w-full max-[992px]:pb-4">
+          <p className="flex justify-between items-center w-full uppercase g-font-3 text-xs tracking-widest py-3 max-[992px]:text-sm cursor-pointer">
+            About us
+          </p>
+          <div
+            className={`flex max-[992px]:flex-col transition-all duration-300 overflow-hidden`}
+          >
+            <p className="pt-1 text-xs tracking-widest uppercase g-font-1 line-height-lg">
+              We have over 22 years of experience in building and home
+              constructions, planning, designing, landscaping and all kinds of
+              civil engineering services.
+            </p>
+          </div>
+        </div>
+        <div className="h-full w-1/4  max-[992px]:px-4 pt-4 max-[992px]:w-full max-[992px]:pb-4">
+          <p className="flex justify-between items-center w-full uppercase g-font-3 text-xs tracking-widest py-3 max-[992px]:text-sm cursor-pointer">
+            Contact Details
+          </p>
+          <div
+            className={`flex max-[992px]:flex-col transition-all duration-300 overflow-hidden`}
+          >
+            <ul className="flex flex-col w-full gap-4 pt-3 text-xs tracking-widest g-font-1">
+              <li>Address:</li>
+              <li>NCI BUILDING</li>
+              <li>No. 381 A,Katugastota Road,Kandy</li>
+              <li className="mt-3">Phone No:</li>
+              <li className="transition-all delay-75 hover:text-blue-300">
+                <a href="tel:812213613">+94 81 221 3613</a>
+              </li>
+              <li className="transition-all delay-75 hover:text-blue-300">
+                <a href="tel:777860950">+94 77 786 0950</a>
+              </li>
+              <li className="mt-3">Email:</li>
+              <li className="transition-all delay-75 hover:text-blue-300">
+                <a href="mailto:ncihomes@sltnet.lk">ncihomes@sltnet.lk</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="h-full w-1/4  max-[992px]:px-4 pt-4 max-[992px]:w-full max-[992px]:pb-4 max-[992px]:border-b-[1px]">
           <p
             className="flex justify-between items-center w-full uppercase g-font-3 text-xs tracking-widest py-3 max-[992px]:text-sm cursor-pointer"
-            onClick={() => toggleSection("trendingSearches")}
+            onClick={() => toggleSection("extraLinks")}
           >
             extra links
             <svg
@@ -164,37 +213,33 @@ function Footer() {
           <div
             className={`flex max-[992px]:flex-col transition-all duration-300 overflow-hidden ${
               isSmallScreen
-                ? openSection === "trendingSearches"
+                ? openSection === "extraLinks"
                   ? "max-h-screen"
                   : "max-h-0"
                 : "max-h-screen"
             }`}
           >
-            <ul className="flex flex-col justify-between gap-4 w-1/4 uppercase g-font-1 tracking-widest text-xs pt-3 max-[992px]:w-full">
+            <ul className="grid grid-cols-1 gap-4 w-full uppercase g-font-1 tracking-widest text-xs pt-3 max-[992px]:w-full">
               <li className="py-1 transition-all delay-75 hover:text-blue-300 w-fit">
                 <Link href="/">Home</Link>
               </li>
               <li className="py-1 transition-all delay-75 hover:text-blue-300 w-fit">
                 <Link href="/about">About</Link>
               </li>
-            </ul>
-            <ul className="flex flex-col px-4 max-[992px]:px-0 justify-between gap-4 w-1/4 uppercase g-font-1 tracking-widest text-xs pt-3 max-[992px]:w-full">
+
               <li className="py-1 transition-all delay-75 hover:text-blue-300 w-fit">
                 <Link href="/services">Services</Link>
               </li>
               <li className="py-1 transition-all delay-75 hover:text-blue-300 w-fit">
                 <Link href="/designs">Designs</Link>
               </li>
-            </ul>
-            <ul className="flex flex-col px-4 max-[992px]:px-0 justify-between gap-4 w-1/4 uppercase g-font-1 tracking-widest text-xs pt-3 max-[992px]:w-full">
+
               <li className="py-1 transition-all delay-75 hover:text-blue-300 w-fit">
                 <Link href="/projects">Completed Projects</Link>
               </li>
               <li className="py-1 transition-all delay-75 hover:text-blue-300 w-fit">
                 <Link href="/projects">Ongoing Projects</Link>
               </li>
-            </ul>
-            <ul className="flex flex-col px-4 max-[992px]:px-0 justify-between gap-4 w-1/4 uppercase g-font-1 tracking-widest text-xs pt-3 max-[992px]:w-full">
               <li className="py-1 transition-all delay-75 hover:text-blue-300 w-fit">
                 <Link href="/contact">Contact Us</Link>
               </li>
